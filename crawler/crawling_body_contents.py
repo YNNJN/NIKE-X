9d5b7bf048a17c5b3355f456fd69961b83adff22
YNNJN <dbswls1014@naver.com>
2020-04-09 15:39:52 +0900

    Add Contents Crawler

--- crawler/crawling_body_contents.py
+++ crawler/crawling_body_contents.py
@@ -1,8 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
 import csv
 
+contents_list = []
 with open('data/body_url.csv', 'r', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     for link in reader:
         link = ''.join(map(str, link))
         url = 'https://news.nike.com' + link
-        print(url)
+
+        response = requests.get(url)
+        response = response.text
+        data = BeautifulSoup(response, 'html.parser')
+
+        contents = data.find_all('p', class_='text--p')
+        contents_list.append(contents)
+
+with open('body_contents.csv', 'w', encoding='utf-8', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(contents_list)
