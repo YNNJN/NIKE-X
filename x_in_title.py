074760392eec91652d8e5b0e34ddafbcf23f5357
YNNJN <dbswls1014@naver.com>
2020-04-12 20:11:47 +0900

    Extract Noun Phrases

--- x_in_title.py
+++ x_in_title.py
@@ -1,41 +1,32 @@
 import csv
 import nltk
 import re
+from textblob import TextBlob
 
 
 with open('data/articles.csv', 'r', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
 
     collabos = []
-    
     for info in reader:
         title = info[0]
         if title.find(' ' + 'x' + ' ') >= 0:
             collabos.append(title)
 
-    locas =[]
     for i in range(len(collabos)):
+        collabos[i] = collabos[i].replace(' x ', 'x')
+        
+    collabo_noun_phrases = []
+    for collabo in collabos:
+        collabo = TextBlob(collabo)
+        collabo_noun_phrases.append(collabo.noun_phrases)
 
-        pattern = r' x '
-        iterator = re.finditer(pattern, collabos[i])
-        loca = []
-        for match in iterator:
-            loca.append(match.span()) 
-        # loca = [(s,e),(s,e),(s,e),... ]
-        locas.append(loca) #[loca,loca, ]
-
-    #[print(loca) for loca in locas]
+    # print(collabo_noun_phrases)
     
-    for i in range(len(locas)):
-        loca = locas[i]
-        for (s,e) in loca:
-            print(collabos[i][s:e])
-        print("")
-
-# with open('x_in_title.csv', 'w', encoding='utf-8', newline='') as csvfile:
-#     writer = csv.writer(csvfile)
-#     for noun in title_noun_phrases:
-#         writer.writerow([noun])
+with open('x_in_title.csv', 'w', encoding='utf-8', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for noun in collabo_noun_phrases:
+        writer.writerow([noun])
 
 
 
