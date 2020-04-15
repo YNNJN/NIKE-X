a05683daad255304d05785bedabafcf94f5b6311
YNNJN <dbswls1014@naver.com>
2020-04-15 14:33:00 +0900

    Preprocessing considering branch

--- crawler/crawling_body_contents.py
+++ crawler/crawling_body_contents.py
@@ -12,15 +12,27 @@ with open('data/body_url.csv', 'r', encoding='utf-8') as csvfile:
         response = requests.get(url)
         response = response.text
         data = BeautifulSoup(response, 'html.parser')
-
+        
+        #idx = 0
+        contents_dict = {}
         contents = data.find_all('p', class_='text--p')
-        for content in contents:
-            for child in content.descendants:
-                if child is None:
-                    contents_list.append(child)
-                else:
-                    contents_list.append(child)
-        contents_list.append('.........................................................')
+        if contents:
+            tmp = ''
+            for content in contents:
+                content = content.get_text()
+                tmp += content
+            #idx += 1
+            tmp = tmp.replace('\xa0', '')
+            contents_list.append(tmp)
+        else:
+            tmp = ''
+            contents = data.find_all('p')
+            for content in contents:
+                content = content.get_text()
+                tmp += content
+            #idx += 1
+            tmp = tmp.replace('\xa0', '')
+            contents_list.append(tmp)
 
 with open('body_contents.csv', 'w', encoding='utf-8', newline='') as csvfile:
     writer = csv.writer(csvfile)
