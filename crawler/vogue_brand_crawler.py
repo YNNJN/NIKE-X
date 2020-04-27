e7e5988ed6ea0210460aabf0f20b6b36c5e7c7e1
YNNJN <dbswls1014@naver.com>
2020-04-27 20:42:58 +0900

    Change string format

--- crawler/vogue_brand_crawler.py
+++ crawler/vogue_brand_crawler.py
@@ -13,8 +13,10 @@ for i in range(len(name_list)):
     name = name_list[i].find_all('a')[0].__repr__()
     idx_end = name[9:].index('"')
     brand_name = name[69:idx_end+9]
+    brand_name = brand_name.replace('-', ' ')
     brand_name_list.append(brand_name)
 #print(brand_name_list)
+#print(len(brand_name_list)) => 418
 
 with open('vogue_brand.csv', 'w', encoding='utf-8', newline='') as csvfile:
     writer = csv.writer(csvfile)
